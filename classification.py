6a209513a3d1a6bc31f37e3d4390ecb4b2da9299
Nathan Rapport <n.rapport@gmail.com>
2019-06-10 22:24:56 -0400

    fast lookup benchmarks

---
+++ classification.py
@@ -0,0 +1,76 @@
+# given a word, quickly find the best approximate matching word from a large list
+from trie import Trie
+import time
+from fuzzywuzzy import fuzz 
+from tabulate import tabulate
+from misspellings import word_pairs
+
+def build_trie(words):
+    """ Return a trie built from a set of 235,886 words """
+
+    print('building trie...')
+    root = Trie()
+    for i, word in enumerate(words):
+        root.add(word, i)
+    print('done\n')
+    return root
+
+def find_best_match(misspelling, chunk):
+    """ find best match in a chunk """
+    best_ratio, best_word = 0, '' 
+    ratios = []
+    for word in chunk:
+        ratio = fuzz.ratio(misspelling, word) / 100
+        ratios.append((ratio, word))
+        if ratio > best_ratio:
+            best_ratio = ratio
+            best_word = word
+
+    return best_ratio, best_word
+
+def make_chunk(trie, words, word):
+    """ Chunk of words to search over in political words """
+    idx = trie.find(word)
+    max_bound, min_bound = min(len(words), idx + 50), max(0, idx - 50)
+    return words[min_bound:max_bound] # fuzzy match on 200 names
+
+def main(words, word_pairs):
+    trie = build_trie(words)
+
+    # calculate naive worst case
+    start = time.time()
+    misspelling = 'pronounciation'
+    for word in words:
+        fuzz.ratio(misspelling, word)
+    elapsed = time.time() - start
+    worst_case = round(1000 * elapsed / len(word_pairs), 2)
+
+    # calculate average case with fast lookup
+    rows = []        
+    start = time.time()
+    for correct, misspelling in word_pairs: 
+        chunk = make_chunk(trie, words, misspelling)
+        best_ratio, best_word = find_best_match(misspelling, chunk)
+        rows.append((misspelling, correct, best_word, best_ratio))
+ 
+    elapsed = time.time() - start
+    sorted_rows = sorted(rows, key=lambda x: -x[3])
+    avg_case = round(1000 * elapsed / len(word_pairs), 2)
+    speedup = round(worst_case/avg_case, 2)
+
+    # print results
+    print(tabulate(sorted_rows,
+        headers=[
+            'Misspelled word',
+            'Correct word',
+            'Best match found',
+            'Match ratio (%)'],
+        floatfmt='.3f'))
+    
+    print('\nnaive worst case: {} ms'.format(worst_case))
+    print('avg time/word = {} ms'.format(avg_case))
+    print('speedup over naive case = {}x'.format(speedup))
+
+if __name__ == '__main__':
+    words = open('/usr/share/dict/words').read().splitlines()
+    main(words, word_pairs)
--- trie.py
+++ trie.py
@@ -1,19 +1,20 @@
-# create a trie out of pol_names full names (ignoring spaces)
 import pdb
 import re
 
 class Trie():
+    """ Create a trie from a sorted list of words """
     
     def __init__(self, char='*'):
         self.char = char
         self.children = []
-        self.word_finished = False
         self.idx = None 
         self.words = []
 
-    # add a word to the trie
-    # idx =: index of the word being added
     def add(self, word, idx): 
+        """
+        Add a word to the trie
+        idx =: index of the word being added
+        """
         word = re.sub('\s', '', word)
         self.words += [word]
         node = self
@@ -36,10 +37,8 @@ class Trie():
                 node = new_node
                 node.idx = idx
 
-        node.word_finished = True
-
-    # returns the first index from the set of words that contain the prefix
     def find(self, prefix):
+        """ Returns the first index from the set of words that contain the prefix """
         word = re.sub('\s', '', prefix)
         node = self
         if not self.children:
@@ -62,6 +61,6 @@ if __name__ == "__main__":
     for word in words[:10]:
         print('{}: {}'.format(word, root.find(word)))
 
-    print('{}: {}'.format('  aa', root.find('aa')))
-    print('{}: {}'.format(' aarmed', root.find('aarmed')))
+    print('{}: {}'.format('aa', root.find('aa')))
+    print('{}: {}'.format('aarmed', root.find('aarmed')))
     pdb.set_trace()
